1e76568c107cafa669a6309bc73a37fb28d439ac
mhatib <muhd.hatib@gmail.com>
2016-06-28 13:42:37 +0800

    added fake account and transition to new scene

---
+++ Assets/loginScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class loginScript : MonoBehaviour {
+
+    public Button loginBtn;
+    public InputField loginField;
+    public InputField pwField;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    public void verifyLogin() {
+        if (loginField.text == "admin"){
+            if (pwField.text == "pw")
+            SceneManager.LoadScene("Login Scene");
+        }
+    }
+
+}
