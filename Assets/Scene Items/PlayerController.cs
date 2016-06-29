6c52beba242a83b6794f260cd2be92753d041daa
ifyouaretea <furtado1411@gmail.com>
2016-06-29 13:00:08 +0800

    Score update

---
+++ Assets/Scene Items/PlayerController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public void scoreUpdate(){
+		ScoreManager.score = ScoreManager.score + 1;
+	}
+}
---
+++ Assets/Scene Items/ScoreManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+	public static int score;
+
+	Text text;
+	void Awake(){
+		text = GetComponent<Text> ();
+		score = 0;
+	}
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "Score: " + score;
+	}
+}
