f935d923536f8cfb16cc80bf20dc8b9e4e0cb402
dabin.lee@outlook.com <Chwb5278!>
2016-07-04 23:52:12 +0800

    Login and Registration Implemented

--- Assets/loginScript.cs
+++ Assets/loginScript.cs
@@ -21,13 +21,79 @@ public class loginScript : MonoBehaviour {
     }
 
     public void verifyLogin() {
-        if (loginField.text == "admin"){
-            if (pwField.text == "pw")
-            SceneManager.LoadScene("Login Scene");
+        string url = "http://localhost:8080/noidea/userLogin.php";
+        WWWForm form = new WWWForm();
+        form.AddField("username", loginField.text);
+        form.AddField("userpassword", pwField.text);
+        WWW www = new WWW(url, form);
+
+        StartCoroutine(WaitForRequest(www));
+
+        //if (loginField.text == "admin"){
+        //    if (pwField.text == "pw")
+        //    SceneManager.LoadScene("Login Scene");
+        //}
+        //else
+        //{
+        //    errorMsg.text = "Account not found. Please register.";
+        //}
+    }
+
+    IEnumerator WaitForRequest(WWW www)
+    {
+        yield return www;
+
+         // check for errors
+        if (www.error == null)
+        {
+            if (www.text == "<?False")
+            {
+                Debug.Log("Login Error.");
+            }
+            else
+            {
+
+                SceneManager.LoadScene("Login Scene");
+            }
         }
         else
         {
-            errorMsg.text = "Account not found. Please register.";
+            Debug.Log("WWW Error: " + www.error);
+        }
+    }
+
+    public void register()
+    {
+        string url = "http://localhost:8080/noidea/userRegistration.php";
+        WWWForm form = new WWWForm();
+        form.AddField("username", loginField.text);
+        form.AddField("userpassword", pwField.text);
+        WWW www = new WWW(url, form);
+
+        StartCoroutine(WaitForRequest2(www));
+
+    }
+
+    IEnumerator WaitForRequest2(WWW www)
+    {
+        yield return www;
+
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log(www.text);
+            //if (www.text == "<?False")
+            //{
+            //    Debug.Log("Login Error.");
+            //}
+            //else
+            //{
+            //    SceneManager.LoadScene("Login Scene");
+            //}
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
         }
     }
 
