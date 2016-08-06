2858512568ebabf7f6cfb6291601f95c34add898
mhatib <muhd.hatib@gmail.com>
2016-08-06 23:26:15 +0800

    rotating about islands

---
+++ noIdea2/Assets/Scripts/rotateAbout.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class rotateAbout : MonoBehaviour {
+    public Button island;
+   
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //the sphere gameObject     
+        
+    void FixedUpdate() {
+        //Quaternion target = Quaternion.Euler(10, 0, 0);
+        //transform.rotation = Quaternion.Slerp(enIsland.position, target, 20 * Time.deltaTime);
+        transform.RotateAround(island.transform.position, Vector3.back, 20 * Time.deltaTime);
+    }
+}
