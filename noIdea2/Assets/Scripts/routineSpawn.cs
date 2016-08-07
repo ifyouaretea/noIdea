57b9951d658f517727a2e9f0add9b2063f50daf7
mhatib <muhd.hatib@gmail.com>
2016-08-07 15:27:24 +0800

    added spawn and rotation

---
+++ noIdea2/Assets/Scripts/routineSpawn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class routineSpawn : MonoBehaviour {
+    public GameObject ship;
+
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i < 5; i++)
+        {
+            Instantiate(ship);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+}
