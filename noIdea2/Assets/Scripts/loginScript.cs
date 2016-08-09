4b2e655ef9bb4bf24bd88912ee976cba82c73f70
Dabin Lee <Dabin Lee>
2016-08-09 16:35:41 +0800

    login fixed.

---
+++ noIdea2/Assets/Scripts/loginScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class loginScript : MonoBehaviour {
+
+    public Button loginBtn;
+    public Button RegisterBtn;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    public void verifyLogin() {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void register()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+}
