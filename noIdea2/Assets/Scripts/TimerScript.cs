95c89e6ce3c22ead24e8549e28be218de4469273
Dabin Lee <Dabin Lee>
2016-08-09 18:58:59 +0800

    timer implemented in all levels, 1,2 and 3

---
+++ noIdea2/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour {
+
+    public Text counterText;
+
+    public float timePerRound, seconds;
+
+	// Use this for initialization
+	void Start () {
+        counterText = GetComponent<Text>() as Text;	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        seconds = (int)((timePerRound)-(Time.time % 60f));
+        counterText.text = seconds.ToString("00");
+	}
+}
