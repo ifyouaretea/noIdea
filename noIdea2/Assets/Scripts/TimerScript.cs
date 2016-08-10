4c849155bf5826a4deb6e445c937a8a5618183ad
Dabin Lee <Dabin Lee>
2016-08-11 04:39:31 +0800

    endGame screen added.

--- noIdea2/Assets/Scripts/TimerScript.cs
+++ noIdea2/Assets/Scripts/TimerScript.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TimerScript : MonoBehaviour {
 
@@ -20,6 +21,8 @@ public class TimerScript : MonoBehaviour {
         if (seconds == 0)
         {
             Time.timeScale = 0;
+            SceneManager.LoadScene("endScene");
+
         }
         counterText.text = seconds.ToString("00");
 	}
---
+++ noIdea2/Assets/Scripts/endGameScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class endGameScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    public void playGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
