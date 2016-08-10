1e1c6fa3b0b6656fd36b49dbf7c8d9cb37cb1788
Dabin Lee <Dabin Lee>
2016-08-11 03:37:50 +0800

    slider bar added.

---
+++ noIdea2/Assets/Scripts/allocationSliderText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class allocationSliderText : MonoBehaviour {
+    public Text percentText;
+    public Slider allocationSlider;
+
+	// Use this for initialization
+	void Start () {
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        percentText.text = (allocationSlider.value * 25).ToString("00") + "%";
+	}
+}
