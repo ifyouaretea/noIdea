e6a9fbaf0025defcd394f90d6273ed27297cbf01
Dabin Lee <Dabin Lee>
2016-08-11 08:41:51 +0800

    endgame condition set.

--- noIdea2/Assets/Scripts/endGameScript.cs
+++ noIdea2/Assets/Scripts/endGameScript.cs
@@ -3,19 +3,22 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class endGameScript : MonoBehaviour {
-    public GameObject timer;
-    public GameObject score;
+    public GameObject canvas;
 
-	// Use this for initialization
-	void Start () {
-        //timer = GameObject.Find("Timer");
-        //int secs = timer.GetComponent<timerScript>().
+
+
+    // Use this for initialization
+    void Start () {
 
 
     }
-	
-	// Update is called once per frame
-	void Update () {
+    //island.GetComponent<islandController> ().curr
+    // Update is called once per frame
+    void Update () {
+        if (canvas.GetComponent<playerController> ().maxPopulation == canvas.GetComponent<playerController> ().shipPopulation)
+        {
+            SceneManager.LoadScene("winScreen");
+        }
         if (Input.GetKeyDown(KeyCode.Escape)) { Application.Quit(); }
         //slider.GetComponent<allocationSliderText>().allocationSlider.value;
 
--- noIdea2/Assets/Scripts/menuGameScript.cs
+++ noIdea2/Assets/Scripts/menuGameScript.cs
@@ -6,7 +6,6 @@ public class menuGameScript : MonoBehaviour {
     
 	void Start () {
     }
-	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Escape)) { Application.Quit(); }
